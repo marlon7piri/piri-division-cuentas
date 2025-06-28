8d68e2a37b77a530e7f5427064704c5c9a45d6c5
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-06-28 10:01:30 -0500

    add notification

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,5 +1,7 @@
 'use client'
+import { table } from 'console'
 import React, { FormEvent,FormEventHandler, useEffect, useState, ChangeEvent } from 'react'
+import toast from 'react-hot-toast'
 
 interface ClientesInt {
   id: string
@@ -31,12 +33,13 @@ const Form = () => {
       return [...prevState, { nombre: nombre, monto: 0, id: crypto.randomUUID(), impuesto: 0 }]
     })
     setNombre("")
+    notify()
   }
 
   useEffect(() => {
     setTotal(() => {
       if (!cuenta.iva || !cuenta.subtotal) {
-        return 0.0
+        return null
       } else {
         return parseFloat((cuenta.iva + cuenta.subtotal).toFixed(2))
       }
@@ -44,6 +47,8 @@ const Form = () => {
   }, [cuenta])
 
   const handlerMonto = (value: string, id: string) => {
+
+    
     setClientes((prevState) => {
       const nuevoMonto = parseFloat(value)
       const newArray = prevState.map((l) => {
@@ -53,7 +58,11 @@ const Form = () => {
 
       const suma = newArray.reduce((acc, cliente) => (acc += cliente.monto), 0)
 
-      if (suma > cuenta.subtotal) {
+      if(!total){
+setError("Tienes que tener un total")
+        setIsOk(false)
+
+      }else if (suma > cuenta.subtotal) {
         setError("la suma de los montos supera el subtotal de la cuenta")
         setIsOk(false)
       } else if (suma < cuenta.subtotal) {
@@ -70,70 +79,94 @@ const Form = () => {
     })
   }
 
+  const notify = () => toast.success('Cliente agregado.');
   return (
     <div className='bg-sky-300 w-full min-h-screen py-20'>
       <form
         onSubmit={addPeople}
         className='rounded-md bg-slate-900 text-slate-50 p-10 w-3/4 m-auto flex flex-col gap-5'
       >
-        <label htmlFor=''>Nombre:</label>
+        <label htmlFor=''>Nombre del cliente:</label>
         <input
           value={nombre}
           onChange={(e: ChangeEvent<HTMLInputElement>) => setNombre(e.target.value)}
           type='text'
           placeholder='Jhon Doe ....'
-          className='b-1 bg-slate-50 text-slate-900 p-2 rounded-md'
+          className='b-1 bg-slate-50 text-slate-900 p-2 rounded-md outline-none focus:bg-sky-300'
         />
         <input
           type='submit'
           value='Ingresar'
           className='bg-green-700 px-10 py-4 rounded-md text-slate-900'
         />
-        <label htmlFor=''>Total de la cuenta:</label>
+        {clientes.length === 0 ? <div></div> :(
+            <div className='flex flex-col gap-2'>
+                <label htmlFor=''>Subtotal de la cuenta:</label>
         <input
           type='number'
           value={cuenta.subtotal}
           onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setCuenta({ ...cuenta, subtotal: parseFloat(e.target.value) || 0 })
+            setCuenta({ ...cuenta, subtotal: parseFloat(e.target.value)})
           }
-          placeholder='total de la cuenta'
-          className='b-1 bg-slate-50 text-slate-900 p-2 rounded-md'
+          className='b-1 bg-slate-50 text-slate-900 p-2 rounded-md outline-none focus:bg-sky-300'
         />
         <label htmlFor=''>Impuesto de la cuenta:</label>
         <input
           type='number'
           value={cuenta.iva}
           onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setCuenta({ ...cuenta, iva: parseFloat(e.target.value) || 0 })
+            setCuenta({ ...cuenta, iva: parseFloat(e.target.value) })
           }
-          placeholder='impuesto de la cuenta'
-          className='b-1 bg-slate-50 text-slate-900 p-2 rounded-md'
+          className='b-1 bg-slate-50 text-slate-900 p-2 rounded-md outline-none focus:bg-sky-300'
         />
-        <label className='text-xl'>Total: ${total}</label>
+        <label className='text-xl text-slate-50'>Total: ${total}</label></div>)}
       </form>
 
-      <div className='mt-3'>
+      <div className='mt-3 w-3/4  m-auto'>
         <p className={`${!isOk ? "text-red-700" : "text-green-600"} text-center font-black`}>{error}</p>
       </div>
 
-      <div className='w-full h-auto mt-5'>
+      <div className='w-full h-auto mt-5 p-4'>
         {clientes.length === 0 ? (
           <p className='w-full text-center'>No hay clientes agregados</p>
         ) : (
-          clientes.map((e) => {
+
+            <table className='w-full m-auto '>
+                <thead >
+                    <tr className='grid  grid-cols-3 p-1 bg-slate-900 text-slate-50'>
+                        <th>Nombre</th>
+                        <th>Subtotal a pagar</th>
+                        <th>Impuesto </th>
+                    </tr>
+                </thead>
+                <tbody className='bg-slate-50'>
+                        {
+                        
+        clientes.map((e) => {
             return (
-              <div key={e.id} className='w-3/4 flex p-4 items-center justify-center mt-2 gap-2'>
-                <h3 className='font-black'>{e.nombre}</h3>
-                <input
+              <tr key={e.id} className=' w-full p-1 grid grid-cols-3 border-1 border-b-slate-100'>
+                <td className='text-center'>
+                <h3 className='font-medium text-xl'>{e.nombre}</h3>
+
+                </td>
+                <td className='text-center'>
+                    <input
                   type='number'
                   onChange={(k: ChangeEvent<HTMLInputElement>) => handlerMonto(k.target.value, e.id)}
                   value={e.monto}
-                  className='font-bold w-[80px] outline-none border-b-1 border-slate-800 focus:border-b-green-500'
+                  className='font-bold w-[70px] outline-none border-[0.7px] p-1 border-slate-700 focus:border-green-500'
                 />
-                <p className='text-slate-950'>{e.impuesto ? e.impuesto : 0.0}</p>
-              </div>
+                </td>
+                <td className='text-right'>
+                    <p className='text-slate-950'>{e.impuesto ? e.impuesto : 0.0}</p>
+                    </td>
+              </tr>
             )
           })
+                    }
+                </tbody>
+            </table>
+          
         )}
       </div>
     </div>
